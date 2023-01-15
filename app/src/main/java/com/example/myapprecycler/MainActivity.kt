d8fab18640fe15a62b17a22c71d971dbcab5ca58
TamashovVladimir <v.tamashov@gmail.com>
2023-01-15 22:40:59 +0300

    homeworkRecycler

--- app/src/main/java/com/example/myapprecycler/MainActivity.kt
+++ app/src/main/java/com/example/myapprecycler/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapprecycler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapprecycler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = MovieAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init (){
+        val movieList = ServerData.getMovies()
+        binding.apply {
+            rcView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            rcView.adapter = adapter
+        }
+        adapter.setMovie(movieList)
     }
 }
